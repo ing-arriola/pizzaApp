8d565eaf5604be2b251d55a2fbebc3d8d4c99e8d
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-06 15:40:27 -0600

    using the useEfect hook and loading data from firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,6 +6,23 @@ import IngredientsList from "./IngredientList";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hooksproject-6adb7.firebaseio.com/ingredients.json")
+      .then((res) => res.json())
+      .then((resData) => {
+        const loadedItems = [];
+        for (let key in resData) {
+          console.log(key);
+          loadedItems.push({
+            id: key,
+            title: resData[key].item.title,
+            amount: resData[key].item.amount,
+          });
+        }
+        setUserIngredients(loadedItems);
+      });
+  }, []); //brackets are the second argument which defines how often this useEffect hook will be executed
 
   const addIngredientHandler = (item) => {
     fetch("https://hooksproject-6adb7.firebaseio.com/ingredients.json", {
